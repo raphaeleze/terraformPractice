a9bc332f62c5c6f92b9b448f92c6cfa6bf940aae
Raphael Eze Echimina <raphaeleze@googlemail.com>
2024-04-10 18:35:13 +0200

    Update main.tf

--- 04/main.tf
+++ 04/main.tf
@@ -1,4 +1,4 @@
 module "ec2" {
-  source        = "git::https://github.com/raphaeleze/terrafromModules"
+  source        = "git::https://github.com/raphaeleze/terrafromModules/tree/main/Aws-Ec2"
   instance_type = var.instance_type
 }
